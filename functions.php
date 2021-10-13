069c08e21a81505bfb0cb3f99f9ca9ada5d157d5
Srdjan <stojanovicsrdjan27@gmail.com>
2021-10-13 12:00:56 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -32,3 +32,26 @@ public function dd()
          echo date('d-m-Y',strtotime($min_date));
 }
 
+
+###################################################################
+
+//Get the language used by the client browser:
+
+function get_client_language($availableLanguages, $default='en'){
+	if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+		$langs=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+		foreach ($langs as $value){
+			$choice=substr($value,0,2);
+			if(in_array($choice, $availableLanguages)){
+				return $choice;
+			}
+		}
+	} 
+	return $default;
+}
+
+###################################################################
+
+
+
